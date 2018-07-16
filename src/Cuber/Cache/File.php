515df24093824960d1aada8a7e0a679a62515adc
gocuber <dafei.net@gmail.com>
2018-07-16 19:33:20 +0800

    Cache\File getHitMissData()

--- src/Cuber/Cache/File.php
+++ src/Cuber/Cache/File.php
@@ -13,59 +13,59 @@ use Cuber\Support\Exception;
 class File
 {
 
-	private static $_instance = null;
-	private $_config = null;
+    private static $_instance = null;
+    private $_config = null;
 
-	private function __construct($config = null)
-	{
-    	if (isset($config)) {
-    		$this->setConfig($config);
-    	}
+    private function __construct($config = null)
+    {
+        if (isset($config)) {
+            $this->setConfig($config);
+        }
     }
 
-	public static function connect($key = 'default')
-	{
-	    $conf = Config::fc($key);
+    public static function connect($key = 'default')
+    {
+        $conf = Config::fc($key);
 
-	    $key = md5(serialize($conf));
-	    if (!isset(self::$_instance[$key])) {
-	        self::$_instance[$key] = new self($conf);
-	    }
+        $key = md5(serialize($conf));
+        if (!isset(self::$_instance[$key])) {
+            self::$_instance[$key] = new self($conf);
+        }
 
-	    return self::$_instance[$key];
-	}
+        return self::$_instance[$key];
+    }
 
-	/**
-	 * 获取
-	 *
-	 * @param string $key
-	 * @param string $default
-	 * @return $data
-	 */
-	public function get($key = '', $default = null)
-	{
-		if (empty($key)) {
-		    return $default;
-		}
+    /**
+     * 获取
+     *
+     * @param string $key
+     * @param string $default
+     * @return $data
+     */
+    public function get($key = '', $default = null)
+    {
+        if (empty($key)) {
+            return $default;
+        }
 
-		$file = $this->getFile($key);
-		if (!is_file($file)) {
-		    return $default;
-		}
+        $file = $this->getFile($key);
+        if (!is_file($file)) {
+            return $default;
+        }
 
-		if (false === ($data = file_get_contents($file))) {
-			return $default;
-		}
+        if (false === ($data = file_get_contents($file))) {
+            return $default;
+        }
 
-		$data = unserialize($data);
-		$time = $data['time'];
-		$data = $data['data'];
-		return (0 === $time or $time > time()) ? $data : $default;
-	}
+        $data = unserialize($data);
+        $time = $data['time'];
+        $data = $data['data'];
+        return (0 === $time or $time > time()) ? $data : $default;
+    }
 
-	/**
-	 * 写入
-	 *
+    /**
+     * 写入
+     *
      * @param string $key
      * @param string $value
      * @param int $time
@@ -73,123 +73,153 @@ class File
      * @return bool
      */
     public function set($key = '', $value = '', $time = 0)
-	{
-		if (empty($key)) {
-			return false;
-		}
+    {
+        if (empty($key)) {
+            return false;
+        }
 
-		$file = $this->getFile($key);
-		if (!mk_dir(dirname($file))) {
-			return false;
-		}
+        $file = $this->getFile($key);
+        if (!mk_dir(dirname($file))) {
+            return false;
+        }
 
-		$data = [];
-		$data['time'] = (0 == $time) ? 0 : time() + $time;
-		$data['data'] = $value;
-		$data = serialize($data);
-		if (function_exists('file_put_contents')) {
-			file_put_contents($file, $data);
-		} else {
-			$handle = fopen($file, 'wb');
-			fwrite($handle, $data);
-			fclose($handle);
-		}
-		return true;
-	}
+        $data = [];
+        $data['time'] = (0 == $time) ? 0 : time() + $time;
+        $data['data'] = $value;
+        $data = serialize($data);
+        if (function_exists('file_put_contents')) {
+            file_put_contents($file, $data);
+        } else {
+            $handle = fopen($file, 'wb');
+            fwrite($handle, $data);
+            fclose($handle);
+        }
+        return true;
+    }
 
-	/**
-	 * 删除
-	 *
+    /**
+     * 删除
+     *
      * @param string $key
      * @return bool
      */
     public function del($key = '')
-	{
-		if (empty($key)) {
-			return false;
-		}
+    {
+        if (empty($key)) {
+            return false;
+        }
 
-		$file = $this->getFile($key);
-		if (is_file($file)) {
-		    return @unlink($file);
-		}
-		return true;
-	}
+        $file = $this->getFile($key);
+        if (is_file($file)) {
+            return @unlink($file);
+        }
+        return true;
+    }
 
-	/**
-	 * getMulti
-	 *
-	 * @param array $keys
-	 * @return array
-	 */
-	public function getMulti($keys = [], $default = null)
-	{
-	    if (empty($keys) or !is_array($keys)) {
-	        return false;
-	    }
+    /**
+     * getMulti
+     *
+     * @param array $keys
+     * @return array
+     */
+    public function getMulti($keys = [], $default = null)
+    {
+        if (empty($keys) or !is_array($keys)) {
+            return false;
+        }
 
-	    $data = [];
-	    foreach ($keys as $key) {
-	        $data[$key] = $this->get($key, $default);
-	    }
-	    return $data;
-	}
+        $data = [];
+        foreach ($keys as $key) {
+            $data[$key] = $this->get($key, $default);
+        }
+        return $data;
+    }
 
-	/**
-	 * setMulti
-	 *
-	 * @param array $items
-	 * @param int $time
-	 * @return bool
-	 */
-	public function setMulti($items = [], $time = 3600)
-	{
-	    if (empty($items) or !is_array($items)) {
-	        return false;
-	    }
+    /**
+     * setMulti
+     *
+     * @param array $items
+     * @param int $time
+     * @return bool
+     */
+    public function setMulti($items = [], $time = 3600)
+    {
+        if (empty($items) or !is_array($items)) {
+            return false;
+        }
 
-	    foreach ($items as $key=>$value) {
-	        $this->set($key, $value, $time);
-	    }
-	    return true;
-	}
+        foreach ($items as $key=>$value) {
+            $this->set($key, $value, $time);
+        }
+        return true;
+    }
 
-	/**
-	 * delMulti
-	 *
-	 * @param array $keys
-	 * @return array
-	 */
-	public function delMulti($keys = [])
-	{
-	    if (empty($keys) or !is_array($keys)) {
-	        return false;
-	    }
+    /**
+     * delMulti
+     *
+     * @param array $keys
+     * @return array
+     */
+    public function delMulti($keys = [])
+    {
+        if (empty($keys) or !is_array($keys)) {
+            return false;
+        }
 
-	    foreach ($keys as $key) {
-	        $this->del($key);
-	    }
-	    return true;
-	}
+        foreach ($keys as $key) {
+            $this->del($key);
+        }
+        return true;
+    }
 
-	/**
-	 * 返回缓存文件全路径
-	 *
-	 * @param string $key
-	 * @return string
-	 */
-	public function getFile($key = false)
-	{
-		if (!isset($key)) {
-			return false;
-		}
+    /**
+     * getHitMissData
+     *
+     * @param array $keys ['id'=>'key', 'id'=>'key']
+     *
+     * @return ['hit'=>['id'=>'value'], 'miss'=>['id','id']]
+     */
+    public function getHitMissData($keys = [])
+    {
+        if (empty($keys)) {
+            return [];
+        }
 
-		$md5    = md5($key);
-		$dir    = $this->_config['dir'];
-		$subdir = $this->_config['is_subdir'] ? substr($md5,0,2).'/'.substr($md5,2,2).'/'.substr($md5,4,2).'/' : '';
-		$file   = $dir . $subdir . $key;
-		return $file;
-	}
+        $data = $this->getMulti($keys);
+
+        $keys_flip = array_flip($keys);
+        $miss      = [];
+        $hit       = [];
+        foreach ($data as $key=>$value) {
+            if (isset($value)) {
+                $hit[$keys_flip[$key]] = $value;
+            } else {
+                $miss[] = $keys_flip[$key];
+            }
+        }
+
+        unset($data, $keys_flip);
+        return ['hit'=>$hit, 'miss'=>$miss];
+    }
+
+    /**
+     * 返回缓存文件全路径
+     *
+     * @param string $key
+     * @return string
+     */
+    public function getFile($key = false)
+    {
+        if (!isset($key)) {
+            return false;
+        }
+
+        $md5    = md5($key);
+        $dir    = $this->_config['dir'];
+        $subdir = $this->_config['is_subdir'] ? substr($md5,0,2).'/'.substr($md5,2,2).'/'.substr($md5,4,2).'/' : '';
+        $file   = $dir . $subdir . $key;
+        return $file;
+    }
 
     /**
      * setConfig

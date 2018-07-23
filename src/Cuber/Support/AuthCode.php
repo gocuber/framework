fe38a4c8c65fd020850e5d593ef277da806d92a0
gocuber <dafei.net@gmail.com>
2018-07-23 18:33:19 +0800

    AuthCode

--- src/Cuber/Support/AuthCode.php
+++ src/Cuber/Support/AuthCode.php
@@ -53,7 +53,7 @@ class AuthCode
     public static function code($string = '', $operation = 'DECODE', $key = null, $expiry = 0)
     {
         // 动态密匙长度，相同的明文会生成不同密文就是依靠动态密匙
-        // 加入随机密钥，可以令密文无任何规律，即便是原文和密钥完全相同，加密结果也会每次不同，增大破解难度。
+        // 加入随机密钥，可以令密文无任何规律，即便是原文和密钥完全相同，加密结果也会每次不同，增大破解难度
         // 取值越大，密文变动规律越大，密文变化 = 16 的 $ckey_length 次方
         // 当此值为 0 时，则不产生随机密钥
         $ckey_length = 16;
@@ -69,7 +69,7 @@ class AuthCode
         // 密匙c用于变化生成的密文
         $keyc = $ckey_length ? ($operation == 'DECODE' ? substr($string, 0, $ckey_length): substr(md5(microtime()), -$ckey_length)) : '';
         // 参与运算的密匙
-        $cryptkey = $keya.md5($keya.$keyc);
+        $cryptkey = $keya . md5($keya.$keyc);
         $key_length = strlen($cryptkey);
         // 明文，前10位用来保存时间戳，解密时验证数据有效性，10到26位用来保存$keyb(密匙b)，解密时会通过这个密匙验证数据完整性
         // 如果是解码的话，会从第$ckey_length位开始，因为密文前$ckey_length位保存 动态密匙，以保证解密正确
@@ -79,18 +79,18 @@ class AuthCode
         $box = range(0, 255);
         $rndkey = array();
         // 产生密匙簿
-        for($i = 0; $i <= 255; $i++) {
+        for ($i = 0; $i <= 255; $i++) {
             $rndkey[$i] = ord($cryptkey[$i % $key_length]);
         }
         // 用固定的算法，打乱密匙簿，增加随机性，好像很复杂，实际上并不会增加密文的强度
-        for($j = $i = 0; $i < 256; $i++) {
+        for ($j = $i = 0; $i < 256; $i++) {
             $j = ($j + $box[$i] + $rndkey[$i]) % 256;
             $tmp = $box[$i];
             $box[$i] = $box[$j];
             $box[$j] = $tmp;
         }
         // 核心加解密部分
-        for($a = $j = $i = 0; $i < $string_length; $i++) {
+        for ($a = $j = $i = 0; $i < $string_length; $i++) {
             $a = ($a + 1) % 256;
             $j = ($j + $box[$a]) % 256;
             $tmp = $box[$a];
@@ -99,12 +99,12 @@ class AuthCode
             // 从密匙簿得出密匙进行异或，再转成字符
             $result .= chr(ord($string[$i]) ^ ($box[($box[$a] + $box[$j]) % 256]));
         }
-        if($operation == 'DECODE') {
+        if ($operation == 'DECODE') {
             // substr($result, 0, 10) == 0 验证数据有效性
             // substr($result, 0, 10) - time() > 0 验证数据有效性
             // substr($result, 10, 16) == substr(md5(substr($result, 26).$keyb), 0, 16) 验证数据完整性
             // 验证数据有效性，请看未加密明文的格式
-            if((substr($result, 0, 10) == 0 || substr($result, 0, 10) - time() > 0) && substr($result, 10, 16) == substr(md5(substr($result, 26).$keyb), 0, 16)) {
+            if ((substr($result, 0, 10) == 0 || substr($result, 0, 10) - time() > 0) && substr($result, 10, 16) == substr(md5(substr($result, 26).$keyb), 0, 16)) {
                 return substr($result, 26);
             } else {
                 return '';
@@ -112,7 +112,7 @@ class AuthCode
         } else {
             // 把动态密匙保存在密文里，这也是为什么同样的明文，生产不同密文后能解密的原因
             // 因为加密后的密文可能是一些特殊字符，复制过程可能会丢失，所以用base64编码
-            return $keyc.str_replace('=', '', base64_encode($result));
+            return $keyc . str_replace('=', '', base64_encode($result));
         }
     }
 

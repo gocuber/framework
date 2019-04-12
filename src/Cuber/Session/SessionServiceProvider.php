947f56044b58ebe1288a7b621f9a782d7e652ef5
gocuber <dafei.net@gmail.com>
2019-04-12 17:51:58 +0800

    SessionServiceProvider

--- src/Cuber/Session/SessionServiceProvider.php
+++ src/Cuber/Session/SessionServiceProvider.php
@@ -11,18 +11,29 @@ class SessionServiceProvider
 {
 
     /**
-     * Register the service provider.
+     * 注册 Session 服务
      *
      * @return void
      */
     public function register()
     {
-        app()->singleton('session.store', function ($app) {
-            return $app->make('session')->driver();
+        app()->singleton('session.file', function () {
+            return new \Cuber\Session\FileSessionHandler();
         });
-
-        app()->singleton('session', function ($app) {
-            return new SessionManager($app);
+        app()->singleton('session.cookie', function () {
+            return new \Cuber\Session\CookieSessionHandler();
+        });
+        app()->singleton('session.mysql', function () {
+            return new \Cuber\Session\MysqlSessionHandler();
+        });
+        app()->singleton('session.memcache', function () {
+            return new \Cuber\Session\MemcacheSessionHandler();
+        });
+        app()->singleton('session.redis', function () {
+            return new \Cuber\Session\RedisSessionHandler();
+        });
+        app()->singleton('session', function ($id = null) {
+            return new \Cuber\Session\SessionManager(config('session.driver', 'session.file'), $id);
         });
     }
 
